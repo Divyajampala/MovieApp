1e79b530b3e540dac147a5d7bc7ed21bc5e200d2
Divya_Jampala <www.jampaladivya18@gmail.com>
2022-12-22 15:46:32 +0530

    Service

--- app/src/main/java/com/movie/services/APIService.kt
+++ app/src/main/java/com/movie/services/APIService.kt
@@ -3,10 +3,28 @@ package com.movie.services
 import com.movie.model.Movie
 import io.reactivex.Single
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface APIService {
 
-    @GET("/3/movie/550")
-    fun getMovieList(@Query("api_key") page: String): Single<Movie>
+    @GET("/3/movie/{movie_id}")
+    fun getMovieDetails(
+        @Query("api_key") page: String,
+        @Path("movie_id") movie_id: String
+    ): Single<Movie>
+
+    @GET("/3/movie/{category}")
+    fun getMovieList(
+        @Query("api_key") api_key: String, @Query("page") page: String,
+        @Path("movie_id") movie_id: String
+    ): Single<Movie>
+
+}
+
+enum class MovieCategory {
+    now_playing,
+    popular,
+    top_rated,
+    upcoming
 }
